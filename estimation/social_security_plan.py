48848f8b0cadceaa4b8cd51c770c21dfa000b29e
rboman <rboman@gmail.com>
2025-09-19 14:23:15 -0700

    Started adding values.

--- estimation/social_security_plan.py
+++ estimation/social_security_plan.py
@@ -15,10 +15,15 @@ def mainprog():
     for _, row in sheet_table.iterrows():
         table_data = collections.defaultdict(list)
         start_time = row['compute_start_date'].to_pydatetime()
+        his_start_date = row['his_start_date'].to_pydatetime()
+        her_start_date = row['her_start_date'].to_pydatetime()
         current_time = start_time
         num_months = 12 * row['years']
         for i in range(num_months):
             print(i, current_time)
+            withdraw = 10000
+            if current_time >= hisstart_date:
+                withdraw -= row['his_monthly']
             current_time += datetime.timedelta(days=30)
         # start_time = datetime.datetime(row['compute_start_date'])
         # print('start time', start_time)
