0aa8a2cbfd8355af53ed41f3fa0c12a25936b38e
rboman <rboman@gmail.com>
2025-09-22 11:06:57 -0700

    Computes balance for each month.

--- estimation/social_security_plan.py
+++ estimation/social_security_plan.py
@@ -13,20 +13,32 @@ def mainprog():
     sheet_table = pd.read_excel(input_sheet)
     print(sheet_table)
     for _, row in sheet_table.iterrows():
+        balance = row['assets']
+        return_rate = row['return']
         table_data = collections.defaultdict(list)
-        start_time = row['compute_start_date'].to_pydatetime()
+        start_date = row['compute_start_date'].to_pydatetime()
         his_start_date = row['his_start_date'].to_pydatetime()
         her_start_date = row['her_start_date'].to_pydatetime()
-        current_time = start_time
+        current_date = start_date
         num_months = 12 * row['years']
         for i in range(num_months):
-            print(i, current_time)
+            print(i, current_date)
             withdraw = 10000
-            if current_time >= hisstart_date:
+            if current_date >= his_start_date:
                 withdraw -= row['his_monthly']
-            current_time += datetime.timedelta(days=30)
+            if current_date >= her_start_date:
+                withdraw -= 0.5 * row['his_monthly']
+            current_date += datetime.timedelta(days=30)
+            balance -= withdraw
+            balance += 30. / 365. * return_rate * balance
+            table_data['label'].append(row['label'])
+            table_data['date'].append(current_date)
+            table_data['withdraw'].append(withdraw)
+            table_data['balance'].append(balance)
         # start_time = datetime.datetime(row['compute_start_date'])
         # print('start time', start_time)
+    table = pd.DataFrame(table_data)
+    print(table)
 
 
 mainprog()
