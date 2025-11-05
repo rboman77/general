82d3009bea186de09d93485ceaf624d37ccc7853
rboman <rboman@gmail.com>
2025-11-05 07:43:50 -0800

    Checks input file.

--- pdf_music/brighten_music.py
+++ pdf_music/brighten_music.py
@@ -1,1 +1,9 @@
+import pathlib
+
 import cv2 as cv
+
+data_folder = (pathlib.Path('/mnt') / 'g' / 'My Drive' / 'church' /
+               'music_dec_2025')
+assert data_folder.exists()
+input_pdf_file = data_folder / 'gesu_bambino_orig.pdf'
+assert input_pdf_file.exists()
