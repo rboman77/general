468f0a4e99ea27dc06872456b2f3eb4d602c7c75
rboman <rboman@gmail.com>
2025-11-21 14:26:28 -0800

    Checks brackets.

--- tax_estimation/tax_and_tithe_estimation.py
+++ tax_estimation/tax_and_tithe_estimation.py
@@ -8,10 +8,20 @@ data_folder = (pathlib.Path('/mnt') / 'g' / 'My Drive' / 'finance' /
 tax_json_file = data_folder / 'tax_estimation_2025.json'
 
 
+def tax_from_brackets(brack, amount):
+    # First check brackets for consistency.
+    for i in range(len(brack) - 1):
+        assert brack[i]["high"] == brack[i + 1]["low"]
+    return 0
+
+
 def mainprog():
     with open(tax_json_file, 'r') as load_file:
         tax_data = json.load(load_file)
-    print(json.dumps(tax_data, indent=4))
+    print('checking irs brackets')
+    tax_from_brackets(tax_data['irs_brackets'], 0.)
+    print('checking ca brackets')
+    tax_from_brackets(tax_data['california_brackets'], 0.)
 
 
 mainprog()
