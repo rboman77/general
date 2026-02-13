facce91e2ccb4bb3533c291bb2a8785f40be7e2e
rboman <rboman@gmail.com>
2026-02-13 14:07:18 -0800

    Handle date strings.

--- holding_check/check_fidelity.py
+++ holding_check/check_fidelity.py
@@ -9,8 +9,28 @@ import pandas as pd  # type: ignore
 
 data_folder = (pathlib.Path('/mnt') / 'g' / 'My Drive' / 'finance' /
                '2025_tax' / 'holding_period_2025_fidelity')
-qualified_div_table = pd.read_csv(data_folder / 'qualified_div.csv')
-qualified_div_table = qualified_div_table.fillna(0.)
+
+
+def read_csv_file(file_path) -> pd.DataFrame:
+    result = pd.read_csv(file_path)
+    result = result.fillna(0)
+    if 'Date' not in result.columns:
+        result['Date'] = result['Run Date']
+    date_list = []
+    for _, row in result.iterrows():
+        if re.search('[0-9]{2}/[0-9]{2}/[0-9]{2}', row['Date']):
+            date_format = '%m/%d/%y'
+        else:
+            date_format = '%m/%d/%Y'
+        date_list.append(
+            datetime.datetime.strptime(row['Date'].strip(), date_format))
+    result['Date'] = date_list
+    return result
+
+
+#qualified_div_table = pd.read_csv(data_folder / 'qualified_div.csv')
+#qualified_div_table = qualified_div_table.fillna(0.)
+qualified_div_table = read_csv_file(data_folder / 'qualified_div.csv')
 sub_table = qualified_div_table[qualified_div_table['1b Qualified Dividends'] >
                                 0]
 sub_table = sub_table[['symbol', 'Date', '1b Qualified Dividends']].copy()
