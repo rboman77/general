24d3d305b3d0ae1a37f684fa21f816835195035f
rboman <rboman@gmail.com>
2026-02-13 13:39:27 -0800

    Started to read qualified dividend table.

---
+++ holding_check/check_fidelity.py
@@ -0,0 +1,16 @@
+import collections
+import datetime
+import json
+import pathlib
+import re
+from typing import Any, List, Dict, Optional, Set
+
+import pandas as pd  # type: ignore
+
+data_folder = (pathlib.Path('/mnt') / 'g' / 'My Drive' / 'finance' /
+               '2025_tax' / 'holding_period_2025_fidelity')
+qualified_div_table = pd.read_csv(data_folder / 'qualified_div.csv')
+qualified_div_table = qualified_div_table.fillna(0.)
+for _, row in qualified_div_table.iterrows():
+    if row['1b Qualified Dividends'] > 0:
+        print(row)
