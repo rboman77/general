6d1d2521c41457d33dfdd5fdce85f2546c2a3676
rboman <rboman@gmail.com>
2026-08-13 13:28:48 -0700

    update

--- tax_estimation/rmd_calculation.py
+++ tax_estimation/rmd_calculation.py
@@ -1,4 +1,5 @@
 import collections
+import datetime
 import json
 import pathlib
 import re
@@ -11,11 +12,18 @@ data_folder = (pathlib.Path('/mnt') / 'g' / 'My Drive' / 'finance' /
 tax_json_file = data_folder / 'tax_estimation_2026.json'
 
 rmd_factor_file = data_folder / 'rmd_table.csv'
+rmd_data_file = data_folder / 'rmd_computation_2026.json'
 
 
 def mainprog() -> None:
     rmd_factor_table = pd.read_csv(rmd_factor_file)
+    print('rmd factors')
     print(rmd_factor_table)
+    with open(rmd_data_file, 'r') as load_file:
+        rmd_data = json.load(load_file)
+    for key in ('current_date', ):
+        rmd_data[key] = datetime.datetime.fromisoformat(rmd_data[key])
+    print(rmd_data)
 
 
 mainprog()
